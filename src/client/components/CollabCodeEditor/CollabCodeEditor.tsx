d821b4b48cbf10cc5e7c912b74adf91a59e4e999
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-05 13:10:01 -0800

    DOC: moved documentation from code into README for CollabCodeEditor

--- src/client/components/CollabCodeEditor/CollabCodeEditor.tsx
+++ src/client/components/CollabCodeEditor/CollabCodeEditor.tsx
@@ -10,36 +10,6 @@ Instead, import basicSetup from 'codemirror' and Editorview fromr '@codemirror/v
 This took me a long time to figure out *sadface*
 */
 
-/*
-Useful text snippets:
-    // if you want to use updateListenerExtension, it needs to be included as an extension in CodeMirror
-    let updateListenerExtension = EditorView.updateListener.of((update) => {
-        // only update when the text changes
-        // note that listener fires for all changes, including clicks and cursor movement which does not change text 
-        if (update.docChanged) {
-            const text = update.state.doc.toString();
-            cbSetText(text)
-            console.log('CollaborativeEditor detecting text change: ', text)
-        }
-    });
-
-    ytext.observe(event => {
-        console.log('ytext  modified to ', ytext.toString())
-        console.log('ytext  modified to ', ytext.toString())
-
-      })
-
-    console.log(ydoc.clientID)
-
-    //Example ytext operations
-    ytext.insert(0, 'abc') // insert three elements
-    ytext.format(1, 2, { bold: true }) // delete second element 
-    ytext.toString() // => 'abc'
-    ytext.toDelta() // => [{ insert: 'a' }, { insert: 'bc', attributes: { bold: true }}]
-
-
-*/
-
 // These imports are used for codemirror editor
 import { EditorView, lineNumbers } from '@codemirror/view'; // Do not
 import { basicSetup } from 'codemirror';
@@ -216,7 +186,6 @@ const CollabCodeEditor = ({
     FURTHER READING: https://github.com/yjs/yjs/issues/101
                      https://github.com/yjs/yjs/issues/82
     */
-
   useEffect(() => {
     if (!view) return;
     if (view.state.doc.length !== 0) return;
@@ -225,42 +194,6 @@ const CollabCodeEditor = ({
       changes: [{ from: 0, to: view.state.doc.length, insert: initialText }],
     });
   }, [view, initialText]);
-  /*
-  Explanation of what happens:
-
-  component renders
-    changed:  view, initialTextSet
-  useEffect "setInitialText" fires and shortcircuts because view is undefined
-
-  DOM renders
-    useEffect "setEditorView" fires because editor htmlElement changed
-    and sets editor
-
-  Editor set
-  useEffect "setInitialText" fires.
-    Three shortcircuit checks: editor set (yes, pass)
-                             does editor have text (no, pass)
-                             do we have initial text (no, fail!)
-
-  Parent sets initial text finally (from redux)
-  useEffect "setInitialText" fires.
-    Three shortcircuit checks: editor set (yes, pass)
-                             does editor have text (no, pass)
-                             do we have initial text (yes, fail!)
-  editor text updated
-  
-
-  
-  
-  ytext set
-
-
-
-editorView set --> dispatch
-editorView text mutates
-
-
-*/
 
   return (
     <>
