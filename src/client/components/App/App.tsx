d8eb15c0cbf59ec00d1b0ba20d26153cd534b331
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-05 03:37:01 -0800

    FEAT: adds react router

--- src/client/components/App/App.tsx
+++ src/client/components/App/App.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
 import styles from './App.module.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { defaultMethod } from 'react-router-dom/dist/dom';
+import { v4 as uuid } from 'uuid';
+import Room from '../Room/Room';
+
+// These are the left items on the navbar
+const leftItems = {
+  library: '/room',
+  rooms: '/room',
+};
 
 function App() {
-  return <h1 className={styles.pink}>First commit</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className={`${styles.row} ${styles.navbar}`}>
+          <div className={`${styles.row}`}>
+            {Object.entries(leftItems).map((e) => {
+              const [title, url] = e;
+              return (
+                <div className={styles.margin} key={uuid()}>
+                  <Link to={url}>{title}</Link>
+                </div>
+              );
+            })}
+          </div>
+
+          {/* <div className={styles.row}>
+            <div className={styles.margin}>
+              {userId !== null ? (
+                <a href="http://localhost:8080/auth/logout">logout</a>
+              ) : (
+                <a href={`http://localhost:8080/auth/google`}>log in</a>
+              )}
+            </div>
+          </div> */}
+        </div>
+        <div>
+          <Routes>
+            <Route path="/room" element={<Room />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/client/components/Room/Room.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+
+const Room = () => {
+  return <>asdfsdf</>;
+};
+
+export default Room;
