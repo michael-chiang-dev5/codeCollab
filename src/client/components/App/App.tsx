0e42e29aba82a652c6a629bd7d6eecfd4eb24c36
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-05 03:29:33 -0800

    REFACTOR: moved App component to its own folder

--- src/client/components/App/App.tsx
+++ src/client/components/App/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './App.module.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return <h1 className={styles.pink}>First commit</h1>;
