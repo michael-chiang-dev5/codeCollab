35bd41ac4647c18871f8998b00be9b033ab55882
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-07 19:28:38 -0800

    REFACTOR: cleaned up database access

--- src/server/db/dbPostgreSQL.ts
+++ src/server/db/dbPostgreSQL.ts
@@ -10,8 +10,12 @@ const pool = new Pool({
 pqQuery implements functionality to run a parameterized sql query on a
 postgreSQL database specified by PG_URI. Pool keeps connection open for
 some period of time to assist pooling of queries. 
+Returns an array of objects. Each object is a row in the database
 */
-const pgQuery = (text: string, params: any[]) => {
+const pgQuery = async (
+  text: string,
+  params: any[]
+): Promise<Array<{ [key: string]: any }>> => {
   // This is just to visualize the sql command being sent to the database.
   // It isn't strictly needed but is useful for debugging.
   const sqlCommand = text.replace(/\$(\d+)/g, (match, index) => {
@@ -21,7 +25,9 @@ const pgQuery = (text: string, params: any[]) => {
   });
   console.log('running sql command: ', sqlCommand);
   // Return the result of the sql query
-  return pool.query(text, params);
+  const data = await pool.query(text, params);
+  const rows = data.rows;
+  return rows;
 };
 
 import { MarkdownsWithMetaDataType } from '../../types/types';
@@ -29,8 +35,7 @@ import { MarkdownsWithMetaDataType } from '../../types/types';
 const getMarkdownsWithMetadata = async () => {
   const sql = `SELECT Markdown._id,title,difficulty,str FROM Markdown
     INNER JOIN MarkdownMetadata ON MarkdownMetadata.markdown_id=Markdown._id;`;
-  const data = await pgQuery(sql, []);
-  const rows: MarkdownsWithMetaDataType = data.rows;
+  const rows = (await pgQuery(sql, [])) as MarkdownsWithMetaDataType;
   return rows;
 };
 
@@ -38,11 +43,11 @@ const getMarkdown = async (_id: number) => {
   const sql = `SELECT * 
     FROM Markdown
     WHERE Markdown._id=$1`;
-  const data = await pgQuery(sql, [_id]);
-  if (data.rows.length === 0) {
+  const rows = await pgQuery(sql, [_id]);
+  if (rows.length === 0) {
     return null;
-  } else if (data.rows.length === 1) {
-    return data.rows[0];
+  } else if (rows.length === 1) {
+    return rows[0];
   } else {
     throw `db.getMarkdown: you should get 0 or 1 element when filtering by primary key`;
   }
@@ -65,8 +70,7 @@ const getUsersBySub = async (sub: string): Promise<UserType[]> => {
     const sql = `SELECT * 
     FROM GoogleUserInfo
     WHERE GoogleUserInfo.sub=$1`;
-    const data = await pgQuery(sql, [sub]);
-    const rows: UserType[] = data.rows;
+    const rows = (await pgQuery(sql, [sub])) as UserType[];
     return rows;
   } catch (err) {
     console.log(err);
@@ -86,8 +90,8 @@ const createUser = async (userData: UserType): Promise<UserType> => {
     (sub, picture, email, email_verified)
     VALUES ($1, $2, $3, $4)
     RETURNING *;`;
-    const data = await pgQuery(sql, params);
-    const row: UserType = data.rows[0];
+    const rows = await pgQuery(sql, params);
+    const row = rows[0] as UserType;
     return row;
   } catch (err) {
     console.log('createUser', err);
