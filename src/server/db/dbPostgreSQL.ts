f108c9aaf5774acf4d670f5ccd9f64ef96538503
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-07 18:40:56 -0800

    REFACTOR: fixed error handling in sql database access. we don't need to catch errors here since we want errors to bubble up to routes

--- src/server/db/dbPostgreSQL.ts
+++ src/server/db/dbPostgreSQL.ts
@@ -27,50 +27,53 @@ const pgQuery = (text: string, params: any[]) => {
 import { MarkdownsWithMetaDataType } from '../../types/types';
 
 const getMarkdownsWithMetadata = async () => {
-  try {
-    const sql = `SELECT Markdown._id,title,difficulty,str FROM Markdown
+  const sql = `SELECT Markdown._id,title,difficulty,str FROM Markdown
     INNER JOIN MarkdownMetadata ON MarkdownMetadata.markdown_id=Markdown._id;`;
-    const data = await pgQuery(sql, []);
-    const rows: MarkdownsWithMetaDataType = data.rows;
-    return rows;
-  } catch (err) {
-    console.log(err);
-    return undefined;
-  }
+  const data = await pgQuery(sql, []);
+  const rows: MarkdownsWithMetaDataType = data.rows;
+  return rows;
 };
 
 const getMarkdown = async (_id: number) => {
-  try {
-    const sql = `SELECT * 
+  const sql = `SELECT * 
     FROM Markdown
     WHERE Markdown._id=$1`;
-    const data = await pgQuery(sql, [_id]);
-    if (data.rows.length === 0) {
-      return null;
-    } else if (data.rows.length === 1) {
-      return data.rows[0];
-    } else {
-      throw `db.getMarkdown: you should get 0 or 1 element when filtering by primary key`;
-    }
-  } catch (err) {
-    console.log(err);
-    return undefined;
+  const data = await pgQuery(sql, [_id]);
+  if (data.rows.length === 0) {
+    return null;
+  } else if (data.rows.length === 1) {
+    return data.rows[0];
+  } else {
+    throw `db.getMarkdown: you should get 0 or 1 element when filtering by primary key`;
   }
 };
 
-const getUser = async (sub: string) => {
+/*
+  verify is a callback in passportCreator that is called after google server access the callback url
+  verify has access to user information from google and is used to sync googles auth information to local auth information
+    (1) it calls getUser to see if the user already exists in our local database.
+        Note that we query by the foreign key "sub"
+    (2) There are 3 possibibilities:
+          a: No users are found, ie, this is the first time the user has used the app
+          b: One user is found, ie the user has used the app before
+          c: More than one user is found ,this should not happen since sub should be a primary key for google auth
+*/
+
+import { UserType } from '../../types/types';
+const getUser = async (sub: string): Promise<UserType | null> => {
   try {
     const sql = `SELECT * 
     FROM GoogleUserInfo
     WHERE GoogleUserInfo.sub=$1`;
     const data = await pgQuery(sql, [sub]);
-    if (data.rows.length === 0) {
-      return null;
-    } else if (data.rows.length === 1) {
-      return data.rows[0];
-    } else {
-      throw `db.getUser: more than one user found with sub ${sub} (found ${data.rows.length})`;
-    }
+    const rows: UserType[] = data.rows;
+
+    // 1 user is found in local database. User has used app before
+    if (rows.length === 1) return rows[0];
+    // 0 users found in local database, user is brand new
+    else if (rows.length === 0) return null;
+    // more than 1 user found in the database. This should not happen
+    else throw `db.getUser: more than one user`;
   } catch (err) {
     console.log(err);
     return undefined;
